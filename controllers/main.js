6263dcf1022a969782e1f2fa8cca709cca377687
Lok Leoi <lyulelok@gmail.com>
2024-10-22 14:19:21 -0400

    jwt basics

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const login = async (req, res) => {
 	const { username, password } = req.body
 	if (username === 'admin' && password === 'admin') {
-		const token = jwt.sign({ username }, process.env.SECRET_KEY)
+		const token = jwt.sign({ username }, process.env.JWT_SECRET)
 		res.json({ token })
 	} else {
 		res.status(401).json({ message: 'Invalid credentials' })
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,18 @@
 const jwt = require('jsonwebtoken')
-
 const authMiddleware = (req, res, next) => {
-	const token = req.headers.authorization
-	if (!token) {
+	console.log('header', req.headers)
+	const authHeader = req.headers.authorization
+
+	// Check if the Authorization header exists
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
 		return res.status(401).json({ message: 'Unauthorized' })
 	}
+
+	// Extract the token from the Bearer header
+	const token = authHeader.split(' ')[1] // Split and get the token part
+
 	try {
-		const decoded = jwt.verify(token, process.env.SECRET_KEY)
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
 		req.user = decoded
 		next()
 	} catch (error) {
