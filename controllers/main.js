b2ad65aa210c3041fc8b36d265f732e4a36cd4c3
Lok Leoi <lyulelok@gmail.com>
2024-10-22 16:56:59 -0400

    registration workflow

--- controllers/main.js
+++ controllers/main.js
@@ -1,17 +1,35 @@
+const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const { StatusCodes } = require('http-status-codes')
+
+const register = async (req, res) => {
+	const user = await User.create({ ...req.body })
+	const token = user.createJWT()
+	res.status(StatusCodes.CREATED).json({ user: user.getName(), token })
+}
 
 const login = async (req, res) => {
-	const { username, password } = req.body
-	if (username === 'admin' && password === 'admin') {
-		const token = jwt.sign({ username }, process.env.JWT_SECRET)
-		res.json({ token })
-	} else {
-		res.status(401).json({ message: 'Invalid credentials' })
+	const { email, password } = req.body
+	if (!email || !password) {
+		return res
+			.status(StatusCodes.BAD_REQUEST)
+			.json({ message: 'Please provide email and password' })
 	}
+	const user = await User.findOne({ email })
+	const isPasswordCorrect = await user.matchPassword(password)
+
+	if (!user || !isPasswordCorrect) {
+		return res
+			.status(StatusCodes.UNAUTHORIZED)
+			.json({ message: 'Invalid credentials' })
+	}
+
+	const token = user.createJWT()
+	res.status(StatusCodes.OK).json({ user: user.getName(), token })
 }
 
 const dashboard = async (req, res) => {
 	res.json({ message: 'Welcome to dashboard' })
 }
 
-module.exports = { login, dashboard }
+module.exports = { register, login, dashboard }
