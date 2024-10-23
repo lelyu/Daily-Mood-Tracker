eba583cc19027fbac54e1f2aa4aadcc0c242804f
Lok Leoi <lyulelok@gmail.com>
2024-10-23 18:17:43 -0400

    auth workflow

--- app.js
+++ app.js
@@ -7,13 +7,36 @@ const mainRoute = require('./routes/main')
 const notFound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 const connectDB = require('./db/connect')
+const cookieParser = require('cookie-parser')
 
-// middleware
+// security features
+const rateLimiter = require('express-rate-limit')
+const xss = require('xss-clean')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+app.set('trust proxy', 1)
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000,
+		max: 60,
+	})
+)
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 app.use(express.static('./public'))
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+
 app.use('/api/v1', mainRoute)
-// app.use(notFoundMiddleware)
-// app.use(errorHandlerMiddleware)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000
 
