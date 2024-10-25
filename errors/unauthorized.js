9dea1c39f45bec2e642a35125fe47fcd634f6423
Lok Leoi <lyulelok@gmail.com>
2024-10-24 22:26:10 -0400

    error files

---
+++ errors/unauthorized.js
@@ -0,0 +1,9 @@
+const statusCodes = require('../statusCodes')
+const CustomAPIError = require('./custom-api')
+
+class UnauthorizedError extends CustomAPIError {
+	constructor(message) {
+		super(message)
+		this.statusCode = statusCodes.UNAUTHORIZED
+	}
+}
